21831292d90407833db52ea56349549f1985b8c8
rty-t <bebra.porn@gmail.com>
2025-09-07 17:38:05 +0300

    add git module

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -3,5 +3,12 @@
    enable = true;
    userName = "rty-t";
    userEmail = "bebra.porn@gmail.com";
+   extraConfig = {
+      branch.autosetuprebase = "always";
+      color.ui = true;
+      core.askPass = "";
+      github.user = "rty-t";
+      init.defaultBranch = "main";
+    };
   };
 }
