ef4a0fe52b296d89e25a077c30e1fd52dbf5cec0
rty-t <bebra.porn@gmail.com>
2025-09-17 16:06:29 +0300

    flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  descrpiption = "rtytflake";
+  
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs = { nixpkgs, ... }: {
+    nixosConfigurations.cv01 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./nixos/confirugation.nix
+      ];
+    };
+  };
+}
