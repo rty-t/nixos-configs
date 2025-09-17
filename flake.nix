ab6508997f3f9e414ba9cdb599b8326e8f17b371
rty-t <bebra.porn@gmail.com>
2025-09-17 17:25:55 +0300

    cachyos kernel

--- flake.nix
+++ flake.nix
@@ -3,12 +3,14 @@
   
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
   };
-  outputs = { nixpkgs, ... }: {
+  outputs = { nixpkgs, chaotic, ... }: {
     nixosConfigurations.cv01 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./nixos/configuration.nix
+        chaotic.nixosModules.default
       ];
     };
   };
--- nixos/boot.nix
+++ nixos/boot.nix
@@ -6,9 +6,10 @@
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
    };
-   kernelPackages = pkgs.linuxPackages_xanmod_latest; # Xanmod kernel
+   #kernelPackages = pkgs.linuxPackages_xanmod_latest;# Xanmod kernel
    #kernelPackages = pkgs.linuxPackages_zen;          # Zen kernel
    #kernelPackages = pkgs.linuxPackages_lqx;          # Liquorix kernel
    #kernelPackages = pkgs.linuxPackages_latest;       # Default kernel
+   kernelPackages = pkgs.linuxPackages_cachyos;       # CachyOS kernel
   };
 }
