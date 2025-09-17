1e02c9b7e4cbf993f2502aa1c7d7379e3eb758ed
rty-t <bebra.porn@gmail.com>
2025-09-17 19:57:36 +0300

    home-manager in flake

--- flake.nix
+++ flake.nix
@@ -4,13 +4,26 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+    home-manager = {
+       url = "github:nix-community/home-manager";
+       inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { nixpkgs, chaotic, ... }: {
+  outputs = { nixpkgs, home-manager, chaotic, ... }: 
+  let
+    system = "x86_64-linux";
+  in {
     nixosConfigurations.cv01 = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      inherit system;
       modules = [
         ./nixos/configuration.nix
         chaotic.nixosModules.default
+      ];
+    };
+    homeConfigurations.pyndys = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.${system};
+      modules = [
+        ./home/home.nix
       ];
     };
   };
