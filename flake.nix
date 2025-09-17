4d4e76ce549745e91e2bd05a862c874d1ff4a7d1
rty-t <bebra.porn@gmail.com>
2025-09-17 16:25:45 +0300

    fix flake

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "rtytflake";
   
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
   outputs = { nixpkgs, ... }: {
     nixosConfigurations.cv01 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        ./nixos/confirugation.nix
+        ./nixos/configuration.nix
       ];
     };
   };
