f9ac73f04892c477a7ab674398caa3c682497d80
rty-t <bebra.porn@gmail.com>
2025-09-18 19:17:38 +0300

    vscode

--- home/editors/vscode.nix
+++ home/editors/vscode.nix
@@ -5,9 +5,9 @@
    enable = true;
    package = pkgs.vscodium;
    profiles.pyndys = {
-    extensions = with pkgs; [
-      vscode-extensions.jnoortheen.nix-ide
-      vscode-extensions.github.github-vscode-theme
+    extensions = with pkgs.vscode-extensions; [
+      jnoortheen.nix-ide
+      github.github-vscode-theme
      ];
     userSettings = {
       "files.autoSave" = "on";
