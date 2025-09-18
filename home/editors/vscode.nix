368f212654f826c521853b1a3874252e7659740c
rty-t <bebra.porn@gmail.com>
2025-09-18 19:10:55 +0300

    fix git in vscode

--- home/editors/vscode.nix
+++ home/editors/vscode.nix
@@ -14,6 +14,7 @@
       "workbench.colorTheme" = "GitHub Dark Colorblind (Beta)";
       "git.openRepositoryInParentFolders" = "always";
       "git.confirmSync" = false;
+      "git.enableSmartCommit" = true;
      };
     };
    };
