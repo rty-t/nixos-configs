3e75a9f62ba64710d831bb42b5d98e6f9c8ea3fa
rty-t <bebra.porn@gmail.com>
2025-09-18 20:47:11 +0300

    edit hosts

--- nixos/network/hosts.nix
+++ nixos/network/hosts.nix
@@ -1,5 +1,73 @@
+### dns.malw.link: hosts file
+# Последнее обновление: 13 сентября 2025
 {
   networking.hosts = {
-    "204.12.192.222" = [ "claude.ai" ];
+    "204.12.192.222" = [
+      "claude.ai"
+      #ChatGPT
+      "chatgpt.com"
+      "ab.chatgpt.com"
+      "auth.openai.com"
+      "auth0.openai.com"
+      "platform.openai.com"
+      "cdn.oaistatic.com"
+      "files.oaiusercontent.com"
+      "cdn.auth0.com"
+      "tcr9i.chat.openai.com"
+      "webrtc.chatgpt.com"
+      "android.chat.openai.com"
+      "api.openai.com"
+      "operator.chatgpt.com"
+      "sora.com"
+      "videos.openai.com"
+      #Spotify
+      "api.spotify.com"
+      "xpui.app.spotify.com"
+      "appresolve.spotify.com"
+      "login5.spotify.com"
+      "login.app.spotify.com"
+      "encore.scdn.co"
+      "ap-gew1.spotify.com"
+      "gew1-spclient.spotify.com"
+      "spclient.wg.spotify.com"
+      "api-partner.spotify.com"
+      "aet.spotify.com"
+      "www.spotify.com"
+      "accounts.spotify.com"
+    ];
+    "204.12.192.221" = [ "open.spotify.com" ];
+    ## Anthropic
+    "185.246.223.127" = [
+      "console.anthropic.com"
+      "auth.anthropic.com"
+      "api.anthropic.com"
+    ];
+    "0.0.0.0" = [
+      # IP Logger'ы:
+      "iplogger.org"
+      "wl.gl"
+      "ed.tc"
+      "bc.ax"
+      "maper.info"
+      "2no.co"
+      "yip.su"
+      "iplis.ru"
+      "ezstat.ru"
+      "iplog.co"
+      "grabify.org"
+      # Мусор/реклама:
+      "log16-platform-ycru.tiktokv.com"
+      "adfox.yandex.ru"
+      "adfstat.yandex.ru"
+      "ads-api.tiktok.com"
+      "ads-api.twitter.com"
+      "ads-dev.pinterest.com"
+      "ads-sg.tiktok.com"
+      "an.yandex.ru"
+      "appmetrica.yandex.ru"
+      "mc.yandex.ru"
+      "amc.yandex.ru"
+    ];
   };
 }
+### dns.malw.link: end hosts file
