e93f0052d4eec8b99dd685b0afa7040505bd160b
rty-t <bebra.porn@gmail.com>
2025-09-19 23:52:34 +0300

    update aliases

--- home/cli/fish.nix
+++ home/cli/fish.nix
@@ -7,11 +7,7 @@
       ff = "fastfetch";
       cat = "bat --theme base16";
       du = "dust";
-      home-switch = "home-manager switch --flake ~/nixos-configs/";
-      nixos-switch = "sudo nixos-rebuild switch --flake ~/nixos-configs/";
-      nixos-update = "sudo nixos-rebuild switch --flake ~/nixos-configs/ --upgrade";
-      flake-update = "cd ~/nixos-configs/ && sudo nix flake update";
-      nix-clear = "sudo nix-collect-garbage -d && sudo nix-store --optimise";
+      tree = "broot";
     };
   };
 
