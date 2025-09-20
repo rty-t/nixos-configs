66e8a9880e2634baba62b1ecd46b38d1653ec80d
rty-t <bebra.porn@gmail.com>
2025-09-20 23:53:27 +0300

    add colors in starship

--- home/cli/starship.nix
+++ home/cli/starship.nix
@@ -7,12 +7,12 @@
     settings = {
       add_newline = true;
       character = {
-        success_symbol = ">";
-        error_symbol = "x";  
+        success_symbol = "[❯](bold green)";
+        error_symbol = "[✗](bold red)"; 
       };
       format = lib.concatStrings [
         ">>"
-        "   "
+        "[   ](bold blue)"
         "$directory"
         "$git_branch"
         "$git_status"
