2482dc359cb839306eda6b6c5604afb0eaca2d76
rty-t <bebra.porn@gmail.com>
2025-09-20 23:43:56 +0300

    edit starship

--- home/cli/starship.nix
+++ home/cli/starship.nix
@@ -1,7 +1,25 @@
+{ lib, ...}:
+
 {
   programs.starship = {
     enable = true;
     enableFishIntegration = true;
-    settings = {};
+    settings = {
+      add_newline = true;
+      character = {
+        success_symbol = "➜";
+        error_symbol = "➜";  
+      };
+      format = lib.concatStrings [
+        "░▒▓"
+        "  "
+        ""
+        "$directory"
+        ""
+        "$git_branch"
+        "$git_status"
+        "\n$character"
+      ];
+   };
   };
 }
