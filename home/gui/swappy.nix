cee6d99c6cb03d3e7db1ce6ad028e1a62ebdbbfc
rty-t <bebra.porn@gmail.com>
2025-09-21 20:45:36 +0300

    edit swappy

--- home/gui/swappy.nix
+++ home/gui/swappy.nix
@@ -1,6 +1,13 @@
 {
   programs.swappy = {
     enable = true;
-    settings = {};
+    settings = {
+      Default = {
+        auto_save = false;
+        save_dir = "$HOME/Pictures";
+        save_filename_format = "swappy-%Y%m%d-%H%M%S.png";
+        show_panel = false;
+      };
+    };
   };
 }
