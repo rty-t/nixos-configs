d8e5eee2b26cb6ef7ea70401c9a6ea4895c8f7e1
rty-t <bebra.porn@gmail.com>
2025-09-27 15:46:16 +0300

    hyprland

--- home/hypr/hyprland.nix
+++ home/hypr/hyprland.nix
@@ -2,11 +2,20 @@
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
-      "$mod" = "SUPER";
+      "$mainMod" = "SUPER";
 
-      bindm = [
-        "$mod, T, ghostty"
+      monitor = [",1920x1080@120,auto,1"];
+
+      bind = [
+        "$mainMod, T, exec, ghostty"
+        "$mainMod, Q, killactive,"
       ];
+
+      input = {
+        kb_layout = "ru,us";
+        follow_mouse = 1;
+        kb_options = "grp:alt_shift_toggle";
+      };
     };
   };
 }
