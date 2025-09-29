89b7269ea81ed027621cf399de82c3578d1395b5
rty-t <bebra.porn@gmail.com>
2025-09-29 23:55:41 +0300

    update

--- nixos/software/boot.nix
+++ nixos/software/boot.nix
@@ -5,10 +5,10 @@
       efi.canTouchEfiVariables = true;
     };
     tmp.cleanOnBoot = true;
-    #kernelPackages = pkgs.linuxPackages_xanmod_latest;                                        # Xanmod kernel
+    kernelPackages = pkgs.linuxPackages_xanmod_latest; # Xanmod kernel
     #kernelPackages = pkgs.linuxPackages_zen;                                                  # Zen kernel
     #kernelPackages = pkgs.linuxPackages_lqx;                                                  # Liquorix kernel
     #kernelPackages = pkgs.linuxPackages_latest;                                               # Default kernel
-    kernelPackages = pkgs.linuxPackages_cachyos.cachyOverride {mArch = "GENERIC_V3";}; # CachyOS kernel
+    #kernelPackages = pkgs.linuxPackages_cachyos.cachyOverride {mArch = "GENERIC_V3";};        # CachyOS kernel
   };
 }
