f2cf2cdb9373406f62f1b84ff7816165071fb03d
rty-t <bebra.porn@gmail.com>
2025-10-20 16:17:47 +0300

    update

--- home/cli/git.nix
+++ home/cli/git.nix
@@ -1,14 +1,16 @@
 {
   programs.git = {
     enable = true;
-    userName = "rty-t";
-    userEmail = "email@forconfig";
-    extraConfig = {
+    settings = {
       branch.autosetuprebase = "always";
       color.ui = true;
       core.askPass = "";
       github.user = "rty-t";
       init.defaultBranch = "main";
+      user = {
+        name = "rty-t";
+        email = "email@forconfig";
+      };
     };
   };
 }
