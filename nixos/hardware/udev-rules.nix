ec8aa57607a21301317393b74d5513b6f1bbf482
rty-t <bebra.porn@gmail.com>
2025-11-03 14:37:29 +0300

    edit udev rules

--- nixos/hardware/udev-rules.nix
+++ nixos/hardware/udev-rules.nix
@@ -2,9 +2,6 @@
   services.udev = {
     enable = true;
     extraRules = ''
-      ## Mchose
-      KERNEL=="hidraw*", ATTRS{idVendor}=="258a", MODE="0666"
-
       ## VGN
       KERNEL=="hidraw*", ATTRS{idVendor}=="3554", MODE="0666"
     '';
