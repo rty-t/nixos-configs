276d5235031eb49a56672d2c0aef2dbc51ea89e1
rty-t <bebra.porn@gmail.com>
2025-11-03 16:03:15 +0300

    edit searx

--- nixos/network/searx.nix
+++ nixos/network/searx.nix
@@ -9,7 +9,7 @@
       };
       server = {
         port = 8080;
-        secret_key = "$SEARX_SECRET_KEY";
+        secret_key = "cb1db7f9f96af1fe9b334361e194f8b0b41a487f798ae643b8e1006bbc12ae28";
       };
       ui = {
         default_locale = "ru";
