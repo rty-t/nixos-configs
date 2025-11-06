07e4743f8055a8e84efc05b4ae2c3d9003ee806c
rty-t <bebra.porn@gmail.com>
2025-11-06 19:30:26 +0300

    add stylix and edit git

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     nur.url = "github:nix-community/NUR";
+    stylix.url = "github:nix-community/stylix";
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
     nixvim.url = "github:nix-community/nixvim";
     home-manager.url = "github:nix-community/home-manager";
@@ -13,6 +14,7 @@
     nixpkgs,
     home-manager,
     chaotic,
+    stylix,
     nur,
     nix-flatpak,
     nixvim,
@@ -25,6 +27,7 @@
       modules = [
         ./nixos/configuration.nix
         chaotic.nixosModules.default
+        stylix.nixosModules.stylix
         nix-flatpak.nixosModules.nix-flatpak
       ];
     };
@@ -33,6 +36,7 @@
       modules = [
         ./home/home.nix
         nur.modules.homeManager.default
+        stylix.homeModules.stylix
         nixvim.homeModules.default
       ];
     };
--- home/cli/git.nix
+++ home/cli/git.nix
@@ -5,12 +5,7 @@
       branch.autosetuprebase = "always";
       color.ui = true;
       core.askPass = "";
-      github.user = "rty-t";
       init.defaultBranch = "main";
-      user = {
-        name = "rty-t";
-        email = "email@forconfig";
-      };
     };
   };
 }
