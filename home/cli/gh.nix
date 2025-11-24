b5a22498d3c2b55fdc90b007139f900684966d81
rty-t <bebra.porn@gmail.com>
2025-11-24 23:19:57 +0300

    edit git and gh

--- home/cli/gh.nix
+++ home/cli/gh.nix
@@ -1,10 +1,10 @@
 {
   programs.gh = {
     enable = true;
-    hosts = {
-      "github.com" = {
-        user = "rty-t";
-      };
+    hosts."github.com".user = "rty-t";
+    settings = {
+      editor = "nvim";
+      browser = "zen";
     };
   };
 }
--- home/cli/git.nix
+++ home/cli/git.nix
@@ -7,7 +7,10 @@
       core.askPass = "";
       init.defaultBranch = "main";
       github.user = "rty-t";
-      user.name = "rty-t";
+      user = {
+        name = "rty-t";
+        email = "mikumiku@beam"; # Place with your really email, Hatsune Miku the best btw
+      };
     };
   };
 }
